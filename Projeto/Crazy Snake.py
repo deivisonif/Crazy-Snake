912b467d25ed2133175afe8e9a285ba36eab1f9f
ncorreiaf <113482749+ncorreiaf@users.noreply.github.com>
2023-05-29 11:13:31 -0300

    Update Crazy Snake.py

--- Projeto/Crazy Snake.py
+++ Projeto/Crazy Snake.py
@@ -5,7 +5,6 @@ from random import randint
 import random
 
 pygame.init()
-
 #Declaração de variáveis em geral: 
 
 largura = 640
@@ -22,6 +21,7 @@ y_bloco_preto = randint(50, 430)
 bloco_preto = pygame.Rect(x_bloco_preto, y_bloco_preto, 20, 20)  # Adicione essa linha
 
 comprimentoini = 5
+game_over = 0
 
 xcontrole = 20
 ycontrole = 0
@@ -56,39 +56,38 @@ imagem_maça = pygame.image.load("imagens/imagem_maça.png")
 tela = pygame.display.set_mode((largura, altura),)
 pygame.display.set_caption("Crazy Snake")
 
-#loop
-running = True
-while running:
-    #verificar eventos de entrada
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-            pygame.quit()
-            exit()
-        elif event.type == pygame.KEYDOWN:
-            running = False # Encerrar a tela de início se qualquer tecla for pressionada
-    velocidade = 9 + pontos // 1
-    # desenhar o fundo e a mensagem de inicio na tela
-    tela.blit(tela_inicial_imagem, (0,0))
-    # atualizar tela
-    pygame.display.flip()
-    
-    contador_frames += 1
-    if contador_frames >= intervalo_frames:
-        contador_frames = 0
+def telainicial():
 
-    x_bloco_preto += velocidade_objeto_preto
-    y_bloco_preto += velocidade_objeto_preto
+    global contador_frames, velocidade, x_bloco_preto, y_bloco_preto, largura, altura, velocidade_objeto_preto
+    running = True
+    while running:
+        #verificar eventos de entrada
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+                pygame.quit()
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                running = False # Encerrar a tela de início se qualquer tecla for pressionada
+        velocidade = 9 + pontos // 1
+        # desenhar o fundo e a mensagem de inicio na tela
+        tela.blit(tela_inicial_imagem, (0,0))
+        # atualizar tela
+        pygame.display.flip()
+        
+        contador_frames += 1
+        if contador_frames >= intervalo_frames:
+            contador_frames = 0
 
-    # Verificar se o objeto preto atingiu as bordas da tela
-    if x_bloco_preto < 0 or x_bloco_preto > largura - 20:
-        velocidade_objeto_preto *= -1  # Inverter a direção do movimento
-    if y_bloco_preto < 0 or y_bloco_preto > altura - 20:
-        velocidade_objeto_preto *= -1  # Inverter a direção do movimento
+        x_bloco_preto += velocidade_objeto_preto
+        y_bloco_preto += velocidade_objeto_preto
 
+        # Verificar se o objeto preto atingiu as bordas da tela
+        if x_bloco_preto < 0 or x_bloco_preto > largura - 20:
+            velocidade_objeto_preto *= -1  # Inverter a direção do movimento
+        if y_bloco_preto < 0 or y_bloco_preto > altura - 20:
+            velocidade_objeto_preto *= -1  # Inverter a direção do movimento
 
-relogio = pygame.time.Clock()
-lista_cobra = []
 def aumentacobra(lista_cobra):
     for xey in lista_cobra:
         pygame.draw.rect(tela, (0, 255, 0), (xey[0], xey[1], 20, 20))
@@ -101,136 +100,71 @@ def exibir_pontuacao_final(pontos):
     tela.blit(texto_final, (posicao_x, posicao_y))
     pygame.display.flip()
 
+def jogo():
+    global x_bloco_preto, y_bloco_preto, bloco_preto, x_maca, y_maca, pontos, x_cobra, y_cobra, xcontrole, ycontrole, comprimentoini
+
+    while True:
+        tela.blit(fundo_jogo_imagem, (0, 0))
+        tela.blit(imagem_bloco_preto, (x_bloco_preto, y_bloco_preto))
+        tela.blit(imagem_maça, (x_maca, y_maca))
+        relogio.tick(100)
+        tela.fill((255,255,255))
+        tela.blit(fundo_jogo_imagem, (0, 0))
+        mensagem = f"Pontos: {pontos}"
+        texto_formatado = fonte.render(mensagem, True, (255, 255, 255))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                    if xcontrole == velocidade:
+                        pass
+                    else:
+                        xcontrole = -velocidade
+                        ycontrole = 0
+                if event.key == K_RIGHT:
+                    if xcontrole == -velocidade:
+                        pass
+                    else:
+                        xcontrole = velocidade
+                        ycontrole = 0
+                if event.key == K_UP:
+                    if ycontrole == velocidade:
+                        pass
+                    else:
+                        ycontrole = -velocidade
+                        xcontrole = 0
+                if event.key == K_DOWN:
+                    if ycontrole == -velocidade:
+                        pass
+                    else:
+                        ycontrole = velocidade
+                        xcontrole = 0
+        x_cobra = x_cobra + (xcontrole/10)
+        y_cobra = y_cobra + (ycontrole/10)
+        x_cobra = x_cobra + (xcontrole/10)
+        y_cobra = y_cobra + (ycontrole/10)
+
+        if xcontrole == 0 and ycontrole < 0:
+            imagem_cabeca = imagem_cabeca_cima
+        elif xcontrole == 0 and ycontrole > 0:
+            imagem_cabeca = imagem_cabeca_baixo
+        elif xcontrole < 0 and ycontrole == 0:
+            imagem_cabeca = imagem_cabeca_esquerda
+        elif xcontrole > 0 and ycontrole == 0:
+            imagem_cabeca = imagem_cabeca_direita
 
 
-def reiniciar_jogo():
-    global pontos, x_cobra, y_cobra, lista_cobra, comprimento_inicial, x_maca, y_maca, x_controle, y_controle
-    pontos = 0
-    x_cobra = largura / 2
-    y_cobra = altura / 2
-    lista_cobra = []
-    comprimento_inicial = 5
-    x_maca = randint(40, 600)
-    y_maca = randint(50, 430)
-    x_controle = 20
-    y_controle = 0
-    pygame.mixer.music.play(-1)
-
-while True:
-    tela.blit(fundo_jogo_imagem, (0, 0))
-    tela.blit(imagem_bloco_preto, (x_bloco_preto, y_bloco_preto))
-    tela.blit(imagem_maça, (x_maca, y_maca))
-    relogio.tick(100)
-    tela.fill((255,255,255))
-    tela.blit(fundo_jogo_imagem, (0, 0))
-    mensagem = f"Pontos: {pontos}"
-    texto_formatado = fonte.render(mensagem, True, (255, 255, 255))
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            exit()
-        if event.type == KEYDOWN:
-            if event.key == K_LEFT:
-                if xcontrole == velocidade:
-                    pass
-                else:
-                    xcontrole = -velocidade
-                    ycontrole = 0
-            if event.key == K_RIGHT:
-                if xcontrole == -velocidade:
-                    pass
-                else:
-                    xcontrole = velocidade
-                    ycontrole = 0
-            if event.key == K_UP:
-                if ycontrole == velocidade:
-                    pass
-                else:
-                    ycontrole = -velocidade
-                    xcontrole = 0
-            if event.key == K_DOWN:
-                if ycontrole == -velocidade:
-                    pass
-                else:
-                    ycontrole = velocidade
-                    xcontrole = 0
-    x_cobra = x_cobra + (xcontrole/10)
-    y_cobra = y_cobra + (ycontrole/10)
-    x_cobra = x_cobra + (xcontrole/10)
-    y_cobra = y_cobra + (ycontrole/10)
-
-    if xcontrole == 0 and ycontrole < 0:
-        imagem_cabeca = imagem_cabeca_cima
-    elif xcontrole == 0 and ycontrole > 0:
-        imagem_cabeca = imagem_cabeca_baixo
-    elif xcontrole < 0 and ycontrole == 0:
-        imagem_cabeca = imagem_cabeca_esquerda
-    elif xcontrole > 0 and ycontrole == 0:
-        imagem_cabeca = imagem_cabeca_direita
-
-
-    if x_cobra < 0 or x_cobra > largura or y_cobra < 0 or y_cobra > altura:
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_r:
-                        reiniciar_jogo()
-                    elif event.key == pygame.K_s:
-                        pygame.quit()
-                        exit()
-            tela.blit(fundo_lost_imagem, (0, 0))
-            exibir_pontuacao_final(pontos)
-            pygame.display.flip()
-                
-
-                    
-    cobra = pygame.draw.rect(tela, (0, 255, 0), (x_cobra, y_cobra, 20, 20))
-    maca = tela.blit(imagem_maça, (x_maca, y_maca))
-    bloco = tela.blit(imagem_bloco_preto, (x_bloco_preto, y_bloco_preto))
-
-    if cobra.colliderect(maca):
-        x_maca = randint(40, 600)
-        y_maca = randint(50, 430)
-        pontos = pontos + 1
-        barulhocoli.play()
-        comprimentoini = comprimentoini + 10
-
-    if cobra.colliderect(bloco):
-        x_bloco_preto = randint(40, 600)
-        y_bloco_preto = randint(50, 430)
-        bloco_preto = pygame.Rect(x_bloco_preto, y_bloco_preto, 20, 20)
-        pygame.display.flip()
-        pygame.display.update()
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_r:
-                        reiniciar_jogo()
-                    elif event.key == pygame.K_s:
-                        pygame.quit()
-                        exit()
-            tela.blit(fundo_lost_imagem, (0, 0))
-            exibir_pontuacao_final(pontos)
-            pygame.display.flip()   
-
-
-    lista_cabeca = []
-    lista_cabeca.append(x_cobra)
-    lista_cabeca.append(y_cobra)
-    lista_cobra.append(lista_cabeca)
-
-    if lista_cobra.count(lista_cabeca) > 1:
-        while True:
+        if x_cobra < 0 or x_cobra > largura or y_cobra < 0 or y_cobra > altura:
+            while True:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         exit()
                     elif event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_r:
-                            reiniciar_jogo()
+                        if event.key == pygame.K_r and game_over:
+                            reset_jogo()
+                            game_over = False
                         elif event.key == pygame.K_s:
                             pygame.quit()
                             exit()
@@ -239,17 +173,78 @@ while True:
                 pygame.display.flip()
                     
 
-    if len (lista_cobra) > comprimentoini:
+                        
+        cobra = pygame.draw.rect(tela, (0, 255, 0), (x_cobra, y_cobra, 20, 20))
+        maca = tela.blit(imagem_maça, (x_maca, y_maca))
+        bloco = tela.blit(imagem_bloco_preto, (x_bloco_preto, y_bloco_preto))
 
-        del lista_cobra[0]
-           
-    aumentacobra (lista_cobra)
-    tela.blit(texto_formatado, (420, 40))
-    tela.blit(imagem_cabeca, (x_cobra, y_cobra))
+        if cobra.colliderect(maca):
+            x_maca = randint(40, 600)
+            y_maca = randint(50, 430)
+            pontos = pontos + 1
+            barulhocoli.play()
+            comprimentoini = comprimentoini + 10
 
-    if random.random() < 0.01:  # Ajuste a probabilidade conforme necessário
-        x_bloco_preto = random.randint(40, 600)
-        y_bloco_preto = random.randint(50, 430)
-        bloco_preto = pygame.Rect(x_bloco_preto, y_bloco_preto, 20, 20)
+        if cobra.colliderect(bloco):
+            x_bloco_preto = randint(40, 600)
+            y_bloco_preto = randint(50, 430)
+            bloco_preto = pygame.Rect(x_bloco_preto, y_bloco_preto, 20, 20)
+            pygame.display.flip()
+            pygame.display.update()
+            while True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        exit()
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_r and game_over:
+                            reset_jogo()
+                            game_over = False
+                        elif event.key == pygame.K_s:
+                            pygame.quit()
+                            exit()
+                tela.blit(fundo_lost_imagem, (0, 0))
+                exibir_pontuacao_final(pontos)
+                pygame.display.flip()   
 
-    pygame.display.update()
+
+        lista_cabeca = []
+        lista_cabeca.append(x_cobra)
+        lista_cabeca.append(y_cobra)
+        lista_cobra.append(lista_cabeca)
+
+        if lista_cobra.count(lista_cabeca) > 1:
+            while True:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            exit()
+                        elif event.type == pygame.KEYDOWN:
+                            if event.key == pygame.K_r and game_over:
+                                reset_jogo()
+                                game_over = False
+                            elif event.key == pygame.K_s:
+                                pygame.quit()
+                                exit()
+                    tela.blit(fundo_lost_imagem, (0, 0))
+                    exibir_pontuacao_final(pontos)
+                    pygame.display.flip()
+                        
+
+        if len (lista_cobra) > comprimentoini:
+
+            del lista_cobra[0]
+            
+        aumentacobra (lista_cobra)
+        tela.blit(texto_formatado, (420, 40))
+        tela.blit(imagem_cabeca, (x_cobra, y_cobra))
+
+        if random.random() < 0.01:  # Ajuste a probabilidade conforme necessário
+            x_bloco_preto = random.randint(40, 600)
+            y_bloco_preto = random.randint(50, 430)
+            bloco_preto = pygame.Rect(x_bloco_preto, y_bloco_preto, 20, 20)
+
+        pygame.display.update()
+
+telainicial()
+relogio = pygame.time.Clock()
+lista_cobra = []
+jogo()
